61185e11f0e0a2196efff127d948913fe8ef1815
duykasama <duykasama@gmail.com>
2023-12-20 19:18:50 +0700

    setup basic angular client app

--- NetCore.Microservices.Client/src/app/app.module.ts
+++ NetCore.Microservices.Client/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
