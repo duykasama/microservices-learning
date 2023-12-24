de663cb2eebae004984992d2e2a770b499ac7f95
duykasama <duykasama@gmail.com>
2023-12-24 16:20:48 +0700

    add gpg encryption

---
+++ .script/decrypt_gpg.sh
@@ -0,0 +1,5 @@
+﻿#!/bin/sh
+
+LOCATION=$1
+SECRET_PASSPHRASE=$2
+gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" --output "$LOCATION"/appsettings.Production.json "$LOCATION"/appsettings.Production.json.gpg
