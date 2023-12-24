f6bf68b36cd74b168534d24b257e862c6e58a6f7
duykasama <duykasama@gmail.com>
2023-12-24 16:28:17 +0700

    update decrypt file and publish-packages workflow

--- .script/decrypt_gpg.sh
+++ .script/decrypt_gpg.sh
@@ -1,5 +1,10 @@
 ﻿#!/bin/sh
 
-LOCATION=$1
-SECRET_PASSPHRASE=$2
-gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" --output "$LOCATION"/appsettings.Production.json "$LOCATION"/appsettings.Production.json.gpg
+readonly AUTH_API="NetCore.Microservices.Services.AuthApi"
+readonly COUPON_API="NetCore.Microservices.Services.CouponApi"
+
+apiList=($AUTH_API $COUPON_API)
+
+for api in ${apiList[@]}; do
+  gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" --output "$api"/appsettings.Production.json "$api"/appsettings.Production.json.gpg
+done
